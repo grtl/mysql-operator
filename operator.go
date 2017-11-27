728ae09dac82ae1c286d55ced9afa5eb3d06c7e6
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2017-11-27 16:10:19 +0100

    Cluster controller with dummy informer functions

---
+++ operator.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"k8s.io/client-go/tools/clientcmd"
+
+	"github.com/grtl/mysql-operator/pkg/client/clientset/versioned"
+	"github.com/grtl/mysql-operator/controller"
+)
+
+var (
+	kubeconfig = flag.String("kubeconfig", "", "Path to kubeconfig. Only required if out-of-cluster.")
+	master     = flag.String("master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+)
+
+var Clientset *versioned.Clientset;
+
+func main() {
+	flag.Parse()
+	config, err := clientcmd.BuildConfigFromFlags(*master, *kubeconfig)
+	if (err != nil) {
+		panic(err)
+	}
+
+	Client, err := versioned.NewForConfig(config)
+	if (err != nil) {
+		panic(err)
+	}
+
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	defer cancelFunc();
+
+	clusterController := controller.ClusterController{Client}
+	go clusterController.Run(ctx)
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	for {
+		select {
+		case s := <-signals:
+			fmt.Printf("Received signal %#v\n", s)
+			os.Exit(0)
+		}
+	}
+}
