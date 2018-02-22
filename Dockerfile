30698f72f5cf26433bfe70f926b78fbe36f4d2d4
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2018-02-22 18:17:22 +0100

    Service and stetefulset from template files

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM alpine:latest
 ADD mysql-operator /go/bin/mysql-operator
+ADD artifacts /go/bin/artifacts
 ENTRYPOINT ["/go/bin/mysql-operator"]
--- operator/cluster/operator.go
+++ operator/cluster/operator.go
@@ -1,10 +1,13 @@
 package cluster
 
 import (
+	"bytes"
+	"text/template"
+
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/client-go/kubernetes"
 
 	"github.com/sirupsen/logrus"
@@ -12,145 +15,78 @@ import (
 	crv1 "github.com/grtl/mysql-operator/pkg/apis/cr/v1"
 )
 
-const mySQLPortNumber = 3306
-
 // AddCluster creates the Kubernetes API objects necessary for a MySQL cluster.
-func AddCluster(cluster *crv1.MySQLCluster, kubeClientset kubernetes.Interface) {
-	createServiceForCluster(cluster, kubeClientset)
-	createStatefulSetForCluster(cluster, kubeClientset)
+func AddCluster(cluster *crv1.MySQLCluster, kubeClientset kubernetes.Interface) error {
+	err := createServiceForCluster(cluster, kubeClientset)
+	if err != nil {
+		return err
+	}
+	err = createStatefulSetForCluster(cluster, kubeClientset)
+	return err
 }
 
-func createServiceForCluster(cluster *crv1.MySQLCluster, kubeClientset kubernetes.Interface) {
+func createServiceForCluster(cluster *crv1.MySQLCluster, kubeClientset kubernetes.Interface) error {
 	servicesInterface := kubeClientset.CoreV1().Services(cluster.ObjectMeta.Namespace)
 
-	newService := serviceForCluster(cluster)
-	_, err := servicesInterface.Create(&newService)
+	newService, err := serviceForCluster(cluster)
+	if err != nil {
+		return err
+	}
+
+	_, err = servicesInterface.Create(newService)
 	if err != nil && !apierrors.IsAlreadyExists(err) {
-		logrus.Panic(err)
+		return err
 	} else if apierrors.IsAlreadyExists(err) {
 		logrus.WithFields(logrus.Fields{
 			"cluster": cluster.Name,
 		}).Info("Service for cluster already exists")
 	}
+	return nil
 }
 
-func createStatefulSetForCluster(cluster *crv1.MySQLCluster, kubeClientset kubernetes.Interface) {
+func createStatefulSetForCluster(cluster *crv1.MySQLCluster, kubeClientset kubernetes.Interface) error {
 	statefulSetsInterface := kubeClientset.AppsV1().StatefulSets(cluster.ObjectMeta.Namespace)
 
-	newStatefulSet := statefulSetForCluster(cluster)
-	_, err := statefulSetsInterface.Create(&newStatefulSet)
+	newStatefulSet, err := statefulSetForCluster(cluster)
+	if err != nil {
+		return err
+	}
+
+	_, err = statefulSetsInterface.Create(newStatefulSet)
 	if err != nil && !apierrors.IsAlreadyExists(err) {
-		logrus.Panic(err)
+		return err
 	} else if apierrors.IsAlreadyExists(err) {
 		logrus.WithFields(logrus.Fields{
 			"cluster": cluster.Name,
 		}).Info("StatefulSet for cluster already exists")
 	}
+	return nil
 }
 
-func serviceForCluster(cluster *crv1.MySQLCluster) corev1.Service {
-	return corev1.Service{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: cluster.Spec.Name,
-		},
-		Spec: corev1.ServiceSpec{
-			ClusterIP: "None",
-			Ports: []corev1.ServicePort{
-				{
-					Port: mySQLPortNumber,
-				},
-			},
-			Selector: map[string]string{
-				"mysql-cluster": cluster.Spec.Name,
-			},
-		},
-	}
+func serviceForCluster(cluster *crv1.MySQLCluster) (*corev1.Service, error) {
+	service := new(corev1.Service)
+	err := parseObject(cluster, service, "artifacts/mysql-service.yaml")
+	return service, err
 }
 
-func statefulSetForCluster(cluster *crv1.MySQLCluster) appsv1.StatefulSet {
-	namespace := cluster.ObjectMeta.Namespace
-
-	stsSpec := statefulSetSpecForCluster(cluster)
-
-	return appsv1.StatefulSet{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      cluster.Spec.Name,
-			Namespace: namespace,
-		},
-		Spec: stsSpec,
-	}
+func statefulSetForCluster(cluster *crv1.MySQLCluster) (*appsv1.StatefulSet, error) {
+	statefulSet := new(appsv1.StatefulSet)
+	err := parseObject(cluster, statefulSet, "artifacts/mysql-statefulset.yaml")
+	return statefulSet, err
 }
 
-func statefulSetSpecForCluster(cluster *crv1.MySQLCluster) appsv1.StatefulSetSpec {
-	labels := map[string]string{
-		"mysql-cluster": cluster.Spec.Name,
+func parseObject(cluster *crv1.MySQLCluster, object interface{}, file string) error {
+	tmpl, err := template.ParseFiles(file)
+	if err != nil {
+		return err
 	}
 
-	var replicas int32 = 1
-
-	selector := metav1.LabelSelector{
-		MatchLabels: labels,
+	var stringBuffer string
+	buffer := bytes.NewBufferString(stringBuffer)
+	err = tmpl.Execute(buffer, cluster)
+	if err != nil {
+		return err
 	}
 
-	container := containerForCluster(cluster)
-
-	pvc := pvcForCluster(cluster)
-
-	return appsv1.StatefulSetSpec{
-		Replicas:    &replicas,
-		ServiceName: cluster.Spec.Name,
-		Selector:    &selector,
-		Template: corev1.PodTemplateSpec{
-			ObjectMeta: metav1.ObjectMeta{
-				Labels: labels,
-			},
-			Spec: corev1.PodSpec{
-				Containers: []corev1.Container{
-					container,
-				},
-			},
-		},
-		VolumeClaimTemplates: []corev1.PersistentVolumeClaim{
-			pvc,
-		},
-	}
-}
-
-func containerForCluster(cluster *crv1.MySQLCluster) corev1.Container {
-	envVars := []corev1.EnvVar{
-		{
-			Name:  "MYSQL_ROOT_PASSWORD",
-			Value: cluster.Spec.Password,
-		},
-	}
-
-	return corev1.Container{
-		Name:  cluster.Spec.Name,
-		Image: "mysql:8",
-		Env:   envVars,
-		VolumeMounts: []corev1.VolumeMount{
-			{
-				Name:      "mysql",
-				MountPath: "/var/lib/mysql",
-			},
-		},
-	}
-}
-
-func pvcForCluster(cluster *crv1.MySQLCluster) corev1.PersistentVolumeClaim {
-	return corev1.PersistentVolumeClaim{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "mysql",
-		},
-		Spec: corev1.PersistentVolumeClaimSpec{
-			AccessModes: []corev1.PersistentVolumeAccessMode{
-				"ReadWriteOnce",
-			},
-			Resources: corev1.ResourceRequirements{
-				Requests: corev1.ResourceList{
-					"storage": cluster.Spec.Storage,
-				},
-			},
-		},
-	}
+	return yaml.NewYAMLOrJSONDecoder(buffer, 64).Decode(object)
 }
