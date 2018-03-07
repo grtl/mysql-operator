34e5d694fce7eec646ca3f956f4e8153b62d9201
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2018-03-07 20:21:59 +0100

    Rewrite cluster controller tests to BDD

--- controller/cluster/controller_test.go
+++ controller/cluster/controller_test.go
@@ -1,115 +1,113 @@
-package cluster
+package cluster_test
 
 import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/grtl/mysql-operator/controller/cluster"
+
 	"context"
 	"io/ioutil"
-	"testing"
-	"time"
-
-	"github.com/nauyey/factory"
-	"github.com/stretchr/testify/suite"
 
 	"k8s.io/apimachinery/pkg/watch"
 
+	"github.com/nauyey/factory"
 	"github.com/sirupsen/logrus"
 
 	"github.com/grtl/mysql-operator/controller"
 	crv1 "github.com/grtl/mysql-operator/pkg/apis/cr/v1"
-	testFactory "github.com/grtl/mysql-operator/testing/factory"
+	testingFactory "github.com/grtl/mysql-operator/testing/factory"
 )
 
-type ClusterControllerTestSuite struct {
-	suite.Suite
-
-	cluster *crv1.MySQLCluster
-
-	controller controller.Controller
-	watcher    *watch.FakeWatcher
-	eventsHook controller.EventsHook
-
-	cancelFunc context.CancelFunc
-}
-
-type eventTest func(controller.Event)
-
-const TIMEOUT = time.Second * 1
-
-func (suite *ClusterControllerTestSuite) testWithTimeout(test eventTest) {
-	select {
-	case event := <-suite.eventsHook.GetEventsChan():
-		test(event)
-	case <-time.After(TIMEOUT):
-		suite.Fail("Timeout while waiting for event")
-	}
-}
-
-func (suite *ClusterControllerTestSuite) SetupTest() {
+var _ = Describe("Cluster Controller", func() {
 	// Turn off logging output
 	logrus.SetOutput(ioutil.Discard)
 
-	// Initialize the controller
-	suite.watcher, suite.controller = NewFakeClusterController(16)
-	suite.eventsHook = controller.NewEventsHook(16)
-	err := suite.controller.AddHook(suite.eventsHook)
-	suite.Require().Nil(err)
+	var (
+		cluster *crv1.MySQLCluster
 
-	// Test Cluster
-	suite.cluster = new(crv1.MySQLCluster)
-	err = factory.Build(testFactory.MySQLClusterFactory).To(suite.cluster)
-	suite.Require().Nil(err)
-	suite.watcher.Add(suite.cluster)
+		watcher           *watch.FakeWatcher
+		clusterController controller.Controller
+		eventsHook        controller.EventsHook
+	)
 
-	// Start the controller
-	ctx, cancelFunc := context.WithCancel(context.Background())
-	suite.cancelFunc = cancelFunc
+	BeforeEach(func() {
+		// Initialize the controller
+		watcher, clusterController = NewFakeClusterController(16)
+		eventsHook = controller.NewEventsHook(16)
 
-	go suite.controller.Run(ctx)
-}
-
-func (suite *ClusterControllerTestSuite) TearDownTest() {
-	suite.cancelFunc()
-}
-
-// Test if onAdd function is being called.
-func (suite *ClusterControllerTestSuite) TestClusterController_OnAdd() {
-	suite.testWithTimeout(func(event controller.Event) {
-		suite.Require().Equal(controller.EventAdded, event.Type)
-		suite.Equal(suite.cluster, event.Object.(*crv1.MySQLCluster))
-	})
-}
-
-// Test if onUpdate function is being called.
-func (suite *ClusterControllerTestSuite) TestClusterController_OnUpdate() {
-	// Ignore added event
-	suite.testWithTimeout(func(event controller.Event) {
-		suite.Require().Equal(controller.EventAdded, event.Type)
+		// Setup fake cluster
+		cluster = new(crv1.MySQLCluster)
+		err := factory.Build(testingFactory.MySQLClusterFactory).To(cluster)
+		Expect(err).NotTo(HaveOccurred())
 	})
 
-	// Update cluster
-	suite.watcher.Modify(suite.cluster)
+	JustBeforeEach(func() {
+		err := clusterController.AddHook(eventsHook)
+		Expect(err).NotTo(HaveOccurred())
 
-	suite.testWithTimeout(func(event controller.Event) {
-		suite.Require().Equal(controller.EventUpdated, event.Type)
-		suite.Equal(suite.cluster, event.Object.(*crv1.MySQLCluster))
-	})
-}
-
-// Test if onDelete function is being called.
-func (suite *ClusterControllerTestSuite) TestClusterController_OnDelete() {
-	// Ignore added event
-	suite.testWithTimeout(func(event controller.Event) {
-		suite.Require().Equal(controller.EventAdded, event.Type)
+		watcher.Add(cluster)
 	})
 
-	// Delete cluster
-	suite.watcher.Delete(suite.cluster)
+	When("Cluster is added", func() {
+		It("should get processed by the controller", func(done Done) {
+			var event controller.Event
 
-	suite.testWithTimeout(func(event controller.Event) {
-		suite.Require().Equal(controller.EventDeleted, event.Type)
-		suite.Equal(suite.cluster, event.Object.(*crv1.MySQLCluster))
+			ctx, cancelFunc := context.WithCancel(context.Background())
+			go clusterController.Run(ctx)
+			defer cancelFunc()
+
+			Eventually(eventsHook.GetEventsChan()).Should(Receive(&event))
+			Expect(event.Type).To(Equal(controller.EventAdded))
+			Expect(event.Object).To(Equal(cluster))
+
+			close(done)
+		})
 	})
-}
 
-func TestClusterControllerTestSuite(t *testing.T) {
-	suite.Run(t, new(ClusterControllerTestSuite))
-}
+	When("Cluster is updated", func() {
+		It("should get processed by the controller", func(done Done) {
+			var event controller.Event
+
+			ctx, cancelFunc := context.WithCancel(context.Background())
+			go clusterController.Run(ctx)
+			defer cancelFunc()
+
+			// Ignore added event
+			Eventually(eventsHook.GetEventsChan()).Should(Receive(&event))
+			Expect(event.Type).To(Equal(controller.EventAdded))
+
+			// Update cluster
+			watcher.Modify(cluster)
+
+			Eventually(eventsHook.GetEventsChan()).Should(Receive(&event))
+			Expect(event.Type).To(Equal(controller.EventUpdated))
+			Expect(event.Object).To(Equal(cluster))
+
+			close(done)
+		})
+	})
+
+	When("Cluster is deleted", func() {
+		It("should get processed by the controller", func(done Done) {
+			var event controller.Event
+
+			ctx, cancelFunc := context.WithCancel(context.Background())
+			go clusterController.Run(ctx)
+			defer cancelFunc()
+
+			// Ignore added event
+			Eventually(eventsHook.GetEventsChan()).Should(Receive(&event))
+			Expect(event.Type).To(Equal(controller.EventAdded))
+
+			// Update cluster
+			watcher.Delete(cluster)
+
+			Eventually(eventsHook.GetEventsChan()).Should(Receive(&event))
+			Expect(event.Type).To(Equal(controller.EventDeleted))
+			Expect(event.Object).To(Equal(cluster))
+
+			close(done)
+		})
+	})
+})
---
+++ controller/cluster/fake_test.go
@@ -0,0 +1,11 @@
+package cluster_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	//. "github.com/onsi/gomega"
+	//. "github.com/grtl/mysql-operator/controller/cluster"
+)
+
+var _ = Describe("Fake", func() {
+
+})
