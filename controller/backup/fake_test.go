d78bbe7c53e79135ae5f3d5ac6665d8d18c321ef
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2018-03-07 20:25:12 +0100

    Rewrite backup controller tests to BDD

---
+++ controller/backup/fake_test.go
@@ -0,0 +1,11 @@
+package backup_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	//. "github.com/onsi/gomega"
+	//. "github.com/grtl/mysql-operator/controller/backup"
+)
+
+var _ = Describe("Fake", func() {
+
+})
