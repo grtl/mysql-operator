c050faf7db166211ec69906006f95e3919903ab4
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2018-03-08 18:00:49 +0100

    Run unittests via ginkgo

--- .travis-scripts/build.sh
+++ .travis-scripts/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
 set -e
+set -v
 GOOS=linux GOARCH=amd64 go build -o mysql-operator
 docker build . -t mysql-operator:testing
--- .travis-scripts/install-minikube.sh
+++ .travis-scripts/install-minikube.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 set -e
+set -v
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$K8S_VERSION/bin/linux/amd64/kubectl \
     && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
