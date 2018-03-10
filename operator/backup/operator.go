3f4090cf007805c0129f6351195c0ae011f832cd
aswietlik <swietlik.aga123@gmail.com>
2018-03-10 17:27:21 +0100

    Create PVC for backup

--- operator/backup/operator.go
+++ operator/backup/operator.go
@@ -8,10 +8,12 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/kubernetes"
 	"fmt"
+	"k8s.io/api/core/v1"
 )
 
 const (
 	cronJobTemplate = "artifacts/backup-cronjob.yaml"
+	pvcTemplate = "artifacts/backup-pvc.yaml"
 )
 
 // Operator represents an object to manipulate Backup custom resources.
@@ -31,11 +33,40 @@ func NewBackupOperator(clientset kubernetes.Interface) Operator {
 }
 
 func (b *backupOperator) ScheduleBackup(backup *crv1.MySQLBackup) error {
-	logging.LogBackup(backup).Debug("Creating cron job.")
+	//TODO: error if cluster doesn't exist
+	//TODO: implement OneTime backups
+	//TODO: PVC storage, PVC overriding
+	//TODO: Default name if not provided
 
-	err := b.createCronJob(backup)
+	logging.LogBackup(backup).Debug("Creating PVC.")
+	err := b.createPVC(backup)
 	if err != nil {
 		return err
+	}
+
+	logging.LogBackup(backup).Debug("Creating cron job.")
+	err = b.createCronJob(backup)
+	if err != nil {
+		//TODO: remove PVC
+		return err
+	}
+
+	return nil
+}
+
+func (b *backupOperator) createPVC(backup *crv1.MySQLBackup) error {
+	pvcInterface := b.clientset.CoreV1().PersistentVolumeClaims(backup.Namespace)
+	pvc, err := pvcForBackup(backup)
+	if err != nil {
+		logging.LogBackup(backup).Debug("PVC error: " + fmt.Sprint(err))
+		return err
+	}
+
+	_, err = pvcInterface.Create(pvc)
+	if err != nil && !apierrors.IsAlreadyExists(err) {
+		return err
+	} else if apierrors.IsAlreadyExists(err) {
+		logging.LogBackup(backup).Warn("PVC already exists")
 	}
 
 	return nil
@@ -53,10 +84,16 @@ func (b *backupOperator) createCronJob(backup *crv1.MySQLBackup) error {
 	if err != nil && !apierrors.IsAlreadyExists(err) {
 		return err
 	} else if apierrors.IsAlreadyExists(err) {
-		logging.LogBackup(backup).Warn("Cron job for backup already exists")
+		logging.LogBackup(backup).Warn("Backup already exists")
 	}
 
 	return nil
+}
+
+func pvcForBackup(backup *crv1.MySQLBackup) (*v1.PersistentVolumeClaim, error) {
+	pvc := new(v1.PersistentVolumeClaim)
+	err := util.ObjectFromTemplate(backup, pvc, pvcTemplate)
+	return pvc, err
 }
 
 func cronJobForBackup(backup *crv1.MySQLBackup) (*v1beta1.CronJob, error) {
