20ce021bbb5384737361382e1509960a23c34bde
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2018-03-12 22:21:17 +0100

    Implement fake controller and operator error behaviour

--- controller/cluster/fake.go
+++ controller/cluster/fake.go
@@ -5,21 +5,30 @@ import (
 	kubeFake "k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/testing"
 
-	"github.com/grtl/mysql-operator/controller"
 	operator "github.com/grtl/mysql-operator/operator/cluster"
 	"github.com/grtl/mysql-operator/pkg/client/clientset/versioned/fake"
 )
 
+type FakeClusterController struct {
+	*clusterController
+	*operator.FakeClusterOperator
+}
+
 // NewFakeClusterController returns new operator controller among with prepended
 // watcher. Created controller uses fake clientSets and operator. Size indicates
 // watcher events channel buffer.
-func NewFakeClusterController(size int) (*watch.FakeWatcher, controller.Controller) {
+func NewFakeClusterController(size int) (*watch.FakeWatcher, *FakeClusterController) {
 	kubeClientset := kubeFake.NewSimpleClientset()
 	clientset := fake.NewSimpleClientset()
 
 	watcher := watch.NewFakeWithChanSize(size, false)
 	clientset.PrependWatchReactor("mysqlclusters", testing.DefaultWatchReactor(watcher, nil))
-	fakeController := NewClusterController(clientset, kubeClientset)
-	fakeController.(*clusterController).clusterOperator = operator.NewFakeOperator()
-	return watcher, fakeController
+
+	fakeController := NewClusterController(clientset, kubeClientset).(*clusterController)
+	fakeOperator := operator.NewFakeOperator()
+	fakeController.clusterOperator = fakeOperator
+	return watcher, &FakeClusterController{
+		clusterController:   fakeController,
+		FakeClusterOperator: fakeOperator,
+	}
 }
