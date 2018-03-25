25dd578d59b3ec863ecd537325d3e70671e8bb53
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2018-03-25 22:29:24 +0200

    Tests for template parsing and name functions

--- operator/backup/util_test.go
+++ operator/backup/util_test.go
@@ -2,8 +2,26 @@ package backup_test
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/grtl/mysql-operator/operator/backup"
 )
 
 var _ = Describe("Util", func() {
+	const backupName = "my-backup"
+	const anotherBackupName = "another-backup"
 
+	Describe("CronJobName", func() {
+		It("should generate a name for the cron job", func() {
+			Expect(CronJobName(backupName)).To(Equal("my-backup-job"))
+			Expect(CronJobName(anotherBackupName)).To(Equal("another-backup-job"))
+		})
+	})
+
+	Describe("PVCName", func() {
+		It("should generate a name for the pvc", func() {
+			Expect(PVCName(backupName)).To(Equal("my-backup"))
+			Expect(PVCName(anotherBackupName)).To(Equal("another-backup"))
+		})
+	})
 })
