33310fbffde15803f44cd90f3b5410be421fb809
aswietlik <swietlik.aga123@gmail.com>
2018-04-05 13:03:28 +0200

    Cli backup create

--- cli/cmd/backup/create.go
+++ cli/cmd/backup/create.go
@@ -2,33 +2,84 @@ package backup
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/grtl/mysql-operator/cli/config"
+	"github.com/grtl/mysql-operator/cli/options"
+	crv1 "github.com/grtl/mysql-operator/pkg/apis/cr/v1"
 )
 
-var clusterName string
+var (
+	clusterName string
+	backupName  string
+	storage     string
+	time        string
+)
 
 var backupCreateCmd = &cobra.Command{
 	Use:   "create [backup name]",
-	Short: "short description of backup create",
-	Long: `A longer description of backup create with examples:
+	Short: "Schedules a new MySQL backup",
+	Long: `Schedules a new MySQL backup for specified cluster. The backups will
+be created according to cron-style time provided.
 msp backup create "my-backup" --cluster "my-cluster"`,
+	Args: cobra.MaximumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		backup := "mysql-backup"
-		if len(args) >= 1 {
-			backup = args[0]
+		options := options.ExtractOptions(cmd)
+
+		if backupName == "" {
+			backupName = clusterName
 		}
 
-		//TODO: create backup logic
-		fmt.Println(backup)
-		fmt.Println("backup create--cluster called")
+		err := createMySQLBackup(options)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
 	},
 }
 
 func init() {
 	Cmd.AddCommand(backupCreateCmd)
 
-	backupCreateCmd.Flags().StringVar(&clusterName, "cluster", "",
-		"name of cluster for which the backup is made")
+	backupCreateCmd.Flags().StringVarP(&clusterName, "cluster", "c", "",
+		"name of the cluster to be backed up")
 	backupCreateCmd.MarkFlagRequired("cluster")
+	backupCreateCmd.Flags().StringVarP(&time, "time", "t", "", "cron-style time")
+	backupCreateCmd.MarkFlagRequired("time")
+	backupCreateCmd.Flags().StringVarP(&storage, "storage", "s", "1Gi", "storage value")
+	backupCreateCmd.Flags().StringVar(&storage, "name", "", "backup name")
+}
+
+func createMySQLBackup(options *options.Options) error {
+	fmt.Printf("Creating: %s/%s for %s\n", options.Namespace, backupName, clusterName)
+
+	var (
+		storageQuantity resource.Quantity
+		err             error
+	)
+
+	if storage != "" {
+		storageQuantity, err = resource.ParseQuantity(storage)
+		if err != nil {
+			return err
+		}
+	}
+
+	mySQLBackupInterface := config.GetConfig().Clientset().CrV1().MySQLBackups(options.Namespace)
+	_, err = mySQLBackupInterface.Create(&crv1.MySQLBackup{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: backupName,
+		},
+		Spec: crv1.MySQLBackupSpec{
+			Cluster: clusterName,
+			Time:    time,
+			Storage: storageQuantity,
+		},
+	})
+
+	return err
 }
