cad7bb615de567ebc501db19b7667a25a3ebefb1
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2018-04-06 16:57:24 +0200

    Backup factory

--- testing/factory/backup.go
+++ testing/factory/backup.go
@@ -7,6 +7,7 @@ import (
 	"github.com/nauyey/factory/def"
 
 	crv1 "github.com/grtl/mysql-operator/pkg/apis/cr/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 )
 
 // MySQLBackupFactory generates cluster with random data for testing.
@@ -15,4 +16,23 @@ var MySQLBackupFactory = def.NewFactory(crv1.MySQLBackup{}, "",
 		return fmt.Sprintf("backup-%s", randomdata.RandStringRunes(16)), nil
 	}),
 	def.Field("ObjectMeta.Namespace", "default"),
+	def.DynamicField("Spec.Time", func(model interface{}) (interface{}, error) {
+		minute := randomAny(randomdata.Number(0, 59))
+		hour := randomAny(randomdata.Number(0, 23))
+		day := randomAny(randomdata.Number(1, 31))
+		month := randomAny(randomdata.Number(1, 12))
+		weekday := randomAny(randomdata.Number(1, 7))
+		year := randomAny(randomdata.Number(1900, 3000))
+		return fmt.Sprintf("%s %s %s %s %s %s", minute, hour, day, month, weekday, year), nil
+	}),
+	def.Trait("ChangeDefaults",
+		def.Field("Spec.Storage", resource.MustParse("1Gi")),
+	),
 )
+
+func randomAny(value int) string {
+	if randomdata.Boolean() {
+		return "*"
+	}
+	return fmt.Sprintf("%d", value)
+}
