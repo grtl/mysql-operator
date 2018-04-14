ee5f86893c2055f219cfe92e51eecf381bc9401f
Jakub Sarzynski <sarzynskijakub@gmail.com>
2018-04-14 15:16:01 +0200

    CLI cluster update

--- pkg/cmd/cluster/update.go
+++ pkg/cmd/cluster/update.go
@@ -1,34 +1,64 @@
 package cluster
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/grtl/mysql-operator/pkg/cmd/util/config"
+	"github.com/grtl/mysql-operator/pkg/cmd/util/fail"
+	"github.com/grtl/mysql-operator/pkg/cmd/util/options"
 )
 
-var replicasNumber uint32
+var (
+	portUpdate     int32
+	replicasUpdate int32
+)
 
 var clusterUpdateCmd = &cobra.Command{
 	Use:   "update [cluster name]",
-	Short: "A short description of cluster update",
-	Long: `A longer description of cluster update with usage:
-msp cluster update "my-cluster" --replicas=4`,
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) < 1 {
-			return errors.New("Cluster name is required")
-		}
-		return nil
-	},
+	Short: "Updates a MySQL Cluster",
+	Long: `Updates a MySQL Cluster.
+Specify replicas number and port to update your MySQL Cluster:
+msp cluster update "my-cluster" --replicas 4 --port 1337`,
+	Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		//TODO: update cluster logic
-		fmt.Println("cluster update called")
+		if replicasUpdate != 0 || portUpdate != 0 {
+			options := options.ExtractOptions(cmd)
+			err := updateCluster(args[0], options)
+			if err != nil {
+				fail.Error(err)
+			}
+		}
 	},
 }
 
 func init() {
 	Cmd.AddCommand(clusterUpdateCmd)
 
-	clusterUpdateCmd.Flags().Uint32Var(&replicasNumber, "replicas", 0, "specify number of replicas")
-	clusterUpdateCmd.MarkFlagRequired("replicas")
+	clusterUpdateCmd.Flags().Int32Var(&replicasUpdate, "replicas", 0, "replicas number")
+	clusterUpdateCmd.Flags().Int32Var(&portUpdate, "port", 0, "port number")
+}
+
+func updateCluster(clusterName string, options *options.Options) error {
+	mySQLClusterInterface := config.GetConfig().Clientset().CrV1().MySQLClusters(options.Namespace)
+
+	mySQLCluster, err := mySQLClusterInterface.Get(clusterName, *new(metav1.GetOptions))
+	if err != nil {
+		return err
+	}
+
+	if replicasUpdate != 0 {
+		mySQLCluster.Spec.Replicas = replicasUpdate
+	}
+	if portUpdate != 0 {
+		mySQLCluster.Spec.Port = portUpdate
+	}
+
+	fmt.Printf("Updating: %s/%s\n", options.Namespace, clusterName)
+	_, err = mySQLClusterInterface.Update(mySQLCluster)
+
+	return err
 }
