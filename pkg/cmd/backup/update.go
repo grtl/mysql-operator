10a867367bb65540dd894c49875ee40e758807c3
Jakub Sarzynski <sarzynskijakub@gmail.com>
2018-05-16 16:37:29 +0200

    CLI backup update

--- pkg/cmd/backup/update.go
+++ pkg/cmd/backup/update.go
@@ -1,29 +1,54 @@
 package backup
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/grtl/mysql-operator/pkg/cmd/util/config"
+	"github.com/grtl/mysql-operator/pkg/cmd/util/fail"
+	options "github.com/grtl/mysql-operator/pkg/cmd/util/options"
 )
+
+var timeUpd string
 
 var backupUpdateCmd = &cobra.Command{
 	Use:   "update [backupschedule name]",
-	Short: "A short description of backup update",
-	Long: `A longer description of backup update with usage:
-msp backup update "my-backup"`,
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) < 1 {
-			return errors.New("Backup name is required")
-		}
-		return nil
-	},
+	Short: "Updates a MySQL Backup Schedule",
+	Long: `Updates a MySQL Backup Schedule. Specify time in CRON style to update your backup schedule
+msp backup update "my-backup" --time "59 23 31 DEC Fri *"`,
+	Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		//TODO: update backup logic
-		fmt.Println("backup update called")
+		if timeUpd != "" {
+			options := options.ExtractOptions(cmd)
+			err := updateBackup(args[0], options)
+			if err != nil {
+				fail.Error(err)
+			}
+		}
 	},
 }
 
 func init() {
 	Cmd.AddCommand(backupUpdateCmd)
+
+	backupUpdateCmd.Flags().StringVarP(&timeUpd, "time", "t", "", "CRON style time ")
+}
+
+func updateBackup(backupName string, options *options.Options) error {
+	mySQLBackupInterface := config.GetConfig().Clientset().CrV1().MySQLBackupSchedules(options.Namespace)
+
+	mySQLBackup, err := mySQLBackupInterface.Get(backupName, *new(metav1.GetOptions))
+	if err != nil {
+		return err
+	}
+
+	mySQLBackup.Spec.Time = timeUpd
+
+	fmt.Printf("Updating: %s/%s\n", options.Namespace, backupName)
+	_, err = mySQLBackupInterface.Update(mySQLBackup)
+
+	return err
 }
