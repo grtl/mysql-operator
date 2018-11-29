b50736cccebcdfd8fceec96b035a3abe00444419
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2018-11-29 15:48:08 +0100

    Fix travis minikube bootstraper

--- .travis-scripts/install-minikube.sh
+++ .travis-scripts/install-minikube.sh
@@ -4,5 +4,5 @@ set -e
 set -v
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$K8S_VERSION/bin/linux/amd64/kubectl \
     && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/$MINIKUBE_VERSION/minikube-linux-amd64 \
     && chmod +x minikube && sudo mv minikube /usr/local/bin/
